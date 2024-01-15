8c65e2099fda8d352e4d6e45dff7b2dc27dfc5b2
hitesh <hiteshtripathi12345678@gmail.com>
2024-01-15 17:26:39 +0530

    removed idempotency handling

--- express/server.js
+++ express/server.js
@@ -8,41 +8,6 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const router = express.Router();
-let reqCache = {};
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-async function checkIfResultCached(req, res) {
-  if (reqCache[req.get("x-idempotence-key") + "hasStarted"] != null) {
-    let waitingForResult = true;
-    while (waitingForResult) {
-      let cachedSuccess = reqCache[req.get("x-idempotence-key") + "success"];
-      let cachedError = reqCache[req.get("x-idempotence-key") + "error"];
-      if (cachedSuccess != null) {
-        waitingForResult = false;
-        res.send(cachedSuccess);
-        return true;
-      } else if (cachedError != null) {
-        waitingForResult = false;
-        return true;
-      }
-      // await sleep(1000);
-      function sleep(ms) {
-        return new Promise((resolve) => {
-          setTimeout(resolve, ms);
-        });
-      }
-    }
-    return true;
-  } else {
-    reqCache[req.get("x-idempotence-key") + "hasStarted"] = "started";
-    return false;
-  }
-}
 
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -67,9 +32,6 @@ router.post("/verify", async function (req, res) {
     res.send({ msg: 'request not valid' });
   };
   try {
-    if (await checkIfResultCached(req, res)) {
-      return;
-    }
     const apiKey = `${process.env.SENDGRID_API_KEY}`;
     sgMail.setApiKey(apiKey)
     const msg = {
@@ -79,7 +41,6 @@ router.post("/verify", async function (req, res) {
       text: 'OTP Verification',
       html: '<strong>Hello ' + email + ', Here is your OTP : ' + otp + ' for verification. Do not share this with anyone.</strong>',
     }
-    reqCache[req.get("x-idempotence-key") + "success"] = true;
     await sgMail
       .send(msg)
       .then(() => {
@@ -89,7 +50,6 @@ router.post("/verify", async function (req, res) {
         res.send(error);
       })
   } catch (e) {
-    reqCache[req.get("x-idempotence-key") + "error"] = e; //store the error in the cache
     console.error(e);
   }
 });
