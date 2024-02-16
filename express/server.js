cdb4853a9cdc6a36bb4bb71042ec0c2d39cd5c0b
hitesh <hiteshtripathi12345678@gmail.com>
2024-02-17 01:06:44 +0530

    removed proxy and added idempotency

--- express/server.js
+++ express/server.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 
 const router = express.Router();
 
+let _emails = {};
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello, I am a Test Server.</h1>');
@@ -20,38 +22,48 @@ router.post("/post-check", function (req, res) {
 });
 
 router.post("/check-cache", function (req, res) {
-  res.send(reqCache);
+  res.send(_emails);
 });
 
 router.post("/verify", async function (req, res) {
   var email = req.headers['email'];
   var otp = req.headers['otp'];
   var auth = req.headers['authorization'];
+  var idempotentKey = req.headers['x-idempotency-key'];
   var auth_key = `${process.env.AUTH}`;
   if (auth != auth_key) {
     res.send({ msg: 'request not valid' });
   };
-  try {
-    const apiKey = `${process.env.SENDGRID_API_KEY}`;
-    const fromAddress = `${process.env.SENDGRID_FROM_EMAIL}`;
-    sgMail.setApiKey(apiKey)
-    const msg = {
-      to: email,
-      from: fromAddress,
-      subject: 'BOOM DAO email verification',
-      text: 'OTP Verification',
-      html: '<strong>Your BOOM DAO verification code is ' + otp + '. Do not share this with anyone.</strong>',
-    }
-    await sgMail
-      .send(msg)
-      .then(() => {
+  if (_emails[idempotentKey] == undefined) {
+    try {
+      const apiKey = `${process.env.SENDGRID_API_KEY}`;
+      const fromAddress = `${process.env.SENDGRID_FROM_EMAIL}`;
+      sgMail.setApiKey(apiKey)
+      const msg = {
+        to: email,
+        from: fromAddress,
+        subject: 'BOOM DAO email verification',
+        text: 'OTP Verification',
+        html: '<strong>Your BOOM DAO verification code is ' + otp + '. Do not share this with anyone.</strong>',
+      }
+      if (_emails[idempotentKey] == undefined) {
+        await sgMail
+          .send(msg)
+          .then(() => {
+            _emails[idempotentKey] = true;
+            res.send({ msg: 'email sent successfully.' });
+          })
+          .catch((error) => {
+            res.send(error);
+          })
+      } else {
         res.send({ msg: 'email sent successfully.' });
-      })
-      .catch((error) => {
-        res.send(error);
-      })
-  } catch (e) {
-    console.error(e);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  } else {
+    res.send({ msg: 'email sent successfully.' });
   }
 });
 
